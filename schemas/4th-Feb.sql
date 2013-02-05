d6423a690a25051aa0a73c0bf4313c4deb86e10e
tehlulz <bscarvell@gmail.com>
2013-02-05 13:51:52 +1000

    schema fix

--- schemas/4th-Feb.sql
+++ schemas/4th-Feb.sql
@@ -1,3 +1,2 @@
-ALTER TABLE  `owls` CHANGE  `aspect`  `aspect` ENUM(  'North',  'South',  'East',  'West',  'North-East',  'North-West',  'South-East',  'South-West',  ' ' )
-
+ALTER TABLE  `owls` CHANGE  `aspect`  `aspect` ENUM(  'North',  'South',  'East',  'West',  'North-East',  'North-West',  'South-East',  'South-West',  ' ' );
 ALTER TABLE  `owls` CHANGE  `indoor_features`  `indoor_features` TEXT, CHANGE  `outdoor_features`  `outdoor_features` TEXT, CHANGE  `other_features`  `other_features` TEXT;
